a17a65fa652f60d772befd85fc7f6960527a60ff
Robert Szyszka <robert.szyszka@outlook.com>
2021-04-12 23:57:55 +0200

    Add 18.2 files

---
+++ src/components/features/OrderForm/OrderForm.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import OrderSummary from '../OrderSummary/OrderSummary';
+
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+class OrderForm extends React.Component {
+  render(){
+    return (
+      <Grid>
+        <Row>
+          <Col xs={12}>
+            <OrderSummary></OrderSummary>
+          </Col>
+        </Row>
+      </Grid>
+    );
+  }
+}
+
+export default OrderForm;
